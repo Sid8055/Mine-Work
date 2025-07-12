3b278c39d340c47aa14fbb91bb2e9da1a4003ba0
Sid8055 <157741016+Sid8055@users.noreply.github.com>
2025-07-12 17:38:44 +0530

    Add files via upload

---
+++ Automation_for_psout/app.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import mhi.psout
+import tempfile
+import openpyxl
+
+st.set_page_config("PSOUT Path Graph Viewer", layout="wide")
+
+
+st.markdown("""
+    <style>
+    div.block-container { padding-top: 1rem; }
+    .dashboard-title {
+        font-size: 2rem;
+        font-weight: 700;
+        color: white;
+        margin-bottom: 0.5rem;
+    }
+    .stFileUploader {
+        transform: scale(0.85);
+        width: 95% !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
+
+
+col1, col2, col3 = st.columns(3)
+
+
+with col1:
+    st.markdown('<div class="dashboard-title">PSOUT File Path Plotter</div>', unsafe_allow_html=True)
+
+
+with col2:
+    st.markdown('<div style="font-weight:bold; margin-bottom:0.2rem;">Upload your .psout file</div>', unsafe_allow_html=True)
+    psout_file = st.file_uploader("", type=["psout"], label_visibility="collapsed")
+
+
+with col3:
+    st.markdown('<div style="font-weight:bold; margin-bottom:0.2rem;">Upload your .xlsx file</div>', unsafe_allow_html=True)
+    excel_file = st.file_uploader("", type=["xlsx"], label_visibility="collapsed")
+
+
+if psout_file is not None and excel_file is not None:
+    try:
+        df_sconfg = pd.read_excel(excel_file, sheet_name="SConfig")
+        POI_Parameters = []
+        POI_Parameters_DDL = []
+
+
+        for i in range(1, len(df_sconfg)):
+            POI_Parameters.append('Root/' + df_sconfg['Canvas'][i] + '/' + df_sconfg['ParameterName'][i] + '/0')
+            POI_Parameters_DDL.append(df_sconfg['SignalType'][i] + '_' + df_sconfg['ParameterName'][i])
+
+        
+        selected_path_temp = st.multiselect("Select signals to plot", POI_Parameters_DDL)
+        selected_paths = [POI_Parameters[POI_Parameters_DDL.index(name)] for name in selected_path_temp]
+
+
+        if selected_paths:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".psout") as tmp:
+                tmp.write(psout_file.read())
+                tmp_path = tmp.name
+
+
+            with mhi.psout.File(tmp_path) as file:
+                run = file.run(0)
+
+                
+                fig, ax = plt.subplots(figsize=(10, 5))
+
+                for i, path in enumerate(selected_paths):
+                    tempchannel = file.call(path)
+                    for call in tempchannel.calls():
+                        trace = run.trace(call)
+                        time = trace.domain
+                        ax.plot(time.data, trace.data, label=selected_path_temp[i])
+
+                ax.set_xlabel("Time", color='white', fontsize=18, fontweight='bold')
+                ax.set_ylabel("Value", color='white', fontsize=18, fontweight='bold')
+                ax.set_title("Trace Plot Signals", color='white', fontsize=26, fontweight='bold')
+                ax.grid(True)
+                # ax.legend()
+                legend = ax.legend(frameon=True) 
+                for text in legend.get_texts():
+                    text.set_color("white")
+                    text.set_fontweight("bold")
+                    
+                st.plotly_chart(fig, use_container_width=True)
+        else:
+            st.warning("Please select at least one signal to plot.")
+
+
+    except Exception as e:
+        st.error(f"Error reading or plotting trace: {e}")
